dcd44da12abce37429abb0e2e569069cfdc08233
nycollas <nhenich@gmail.com>
2024-06-04 11:53:05 -0300

    cssupgrade

--- static/script.js
+++ static/script.js
@@ -1,27 +1,27 @@
-//abre e fecha mobile
+// //abre e fecha mobile
 
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+// const menuMobile = document.querySelector('.menu-mobile')
+// const body = document.querySelector('body')
 
-menuMobile.addEventListener('click', () => {
-    menuMobile.classList.contains('bi-list')
-        ? menuMobile.classList.replace('bi-list', 'bi-x')
-        : menuMobile.classList.replace('bi-x', 'bi-list');
-    body.classList.toggle('menu-nav-active')
-})
+// menuMobile.addEventListener('click', () => {
+//     menuMobile.classList.contains('bi-list')
+//         ? menuMobile.classList.replace('bi-list', 'bi-x')
+//         : menuMobile.classList.replace('bi-x', 'bi-list');
+//     body.classList.toggle('menu-nav-active')
+// })
 
-// fecha menu
+// // fecha menu
 
-const navItem = document.querySelectorAll('.nav-item')
+// const navItem = document.querySelectorAll('.nav-item')
 
-navItem.forEach(item => {
-    item.addEventListener('click', () => {
-        if (body.classList.contains('menu-nav-active')) {
-            body.classList.remove('menu-nav-active')
-            menuMobile.classList.replace('bi-x', 'bi-list');
-        }
-    })
-})
+// navItem.forEach(item => {
+//     item.addEventListener('click', () => {
+//         if (body.classList.contains('menu-nav-active')) {
+//             body.classList.remove('menu-nav-active')
+//             menuMobile.classList.replace('bi-x', 'bi-list');
+//         }
+//     })
+// })
 
 const item = document.querySelectorAll('[data-anime]');
 
